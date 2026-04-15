a0b819f316967fb4f67d910261ad695ac95bd175
JoAcO3412 <joaquindevige25@gmail.com>
2026-04-15 16:11:03 -0300

    Create MedioEnvio interface for message sending

---
+++ BridgeMedioEnvio.java
@@ -0,0 +1,12 @@
+//Joaquín Devige.
+//Legajo: 114638.
+//Patrón: Bridge (Estructural).
+
+package patrónbridgeestructural;
+
+
+public interface MedioEnvio {
+    
+    void enviar(String mensaje);
+    
+}
